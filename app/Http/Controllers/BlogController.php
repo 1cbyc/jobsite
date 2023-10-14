a3ee9fac800d59e02d8ed83206eab1f4a23ee4ee
Isaac Emmanuel <35623334+1cbyc@users.noreply.github.com>
2023-10-14 22:54:30 +0100

    Update BlogController.php
    For this blog controller, I added authorization, pagination for listing blogs, validation, updating and deleting blog posts, and responding with appropriate HTTP status codes and messages. I would add the routes and views I did not add earlier, then I would come back to add them here. Check it out Bro. Yusuf.

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,23 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Blog;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class BlogController extends Controller
 {
-    public function __construct()
+    private $blog;
+
+    public function __construct(Blog $blog)
     {
+        $this->blog = $blog;
         $this->middleware('auth:api');
     }
-    // Show all listings
+
     public function index()
     {
-        // return view('blogs.index', [
-        //     'Blogs' => Blog::latest()->paginate(6)
-        // ]);
-
         return response()->json([
-            'blogs' => Blog::all()
+            'blogs' => $this->blog->paginate(10)
         ]);
     }
 
@@ -32,15 +32,43 @@ class BlogController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
-        $blog = $request->validate([
-            'title' => 'required',
-            'content' => 'required',
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
         ]);
 
-        $blog['user_id'] = auth()->id();
+        $validatedData['user_id'] = Auth::id();
 
-        Blog::create($blog);
-        return response()->json(['blog' => $blog], 200);
+        $blog = $this->blog->create($validatedData);
+
+        return response()->json(['blog' => $blog], 201); 
+    }
+
+    public function edit(Blog $blog)
+    {
+        // I would implement a view for editing blog posts after I commit this
+    }
+
+    public function update(Request $request, Blog $blog)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        // I would also add the authorization check here to enable users to edit  blog posts
+
+        $blog->update($validatedData);
+
+        return response()->json(['blog' => $blog], 200); // 200 OK
+    }
+
+    public function destroy(Blog $blog)
+    {
+        // I also want to add an authorization check to allow users delete the blog posts created
+
+        $blog->delete();
+
+        return response()->json(['message' => 'Blog post deleted'], 204);
     }
 }
