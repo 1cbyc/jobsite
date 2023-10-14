f38f2696c014c6ad29ce25c41a58bdc167cc56c7
Isaac Emmanuel <35623334+1cbyc@users.noreply.github.com>
2023-10-14 23:08:11 +0100

    Update web.php
    So far, I updated the routes/api.php, /web.php and the BlogController files. I added the route names to be in sync with the changes I made in the controller. 
    
    Bro, for the /logout route, I've changed it to a POST request so it would be easy to log users out securely (if you don't mind). The delete and edit route I added in the BlogController has been updated here. You can review.

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\UserController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,19 +17,22 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [BlogController::class, 'index']);
-Route::post('/blogs', [BlogController::class, 'store']);
+// The initial Blog routes I updated bro
+Route::get('/', [BlogController::class, 'index'])->name('blogs.index');
+Route::get('/blogs/create', [BlogController::class, 'create'])->name('blogs.create');
+Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store');
+Route::get('/blogs/{blog}', [BlogController::class, 'show'])->name('blogs.show');
+Route::get('/blogs/{blog}/edit', [BlogController::class, 'edit'])->name('blogs.edit');
+Route::put('/blogs/{blog}', [BlogController::class, 'update'])->name('blogs.update');
+Route::delete('/blogs/{blog}', [BlogController::class, 'destroy'])->name('blogs.destroy');
 
-Route::middleware('auth')->group(static function (): void {
-    Route::get('/blogs/create', [BlogController::class, 'create']);
-    Route::post('/logout', [UserController::class, 'logout']);
+// Then, those user auth and registration routes considered
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [UserController::class, 'create'])->name('register');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/login', [UserController::class, 'login'])->name('login');
+    Route::post('/users/authenticate', [UserController::class, 'authenticate'])->name('users.authenticate');
 });
 
+Route::post('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
 
-Route::get('/register', [UserController::class, 'create'])->middleware('guest');
-
-Route::post('/users', [UserController::class, 'store']);
-
-Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
-
-Route::post('/users/authenticate', [UserController::class, 'authenticate']);
